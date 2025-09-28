cf3b8de7941c5a50002ab36afce5d2fb132c8730
ladn0 <84666429+kodika-dangerlyoha@users.noreply.github.com>
2025-09-28 19:23:33 +0300

    28.09.2025-1
    - Изменение фильтров в каталоге
    - Изменение главной кнопки на странице карточки игры

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,6 +1,26 @@
+const form_filter = document.forms.form_filter;
+const inp_search_genres = form_filter.inp_search_genres;
+const inp_search_sorting = form_filter.inp_search_sorting;
+
 const points = [
     {
         'title': "genres",
+        'type_function': "checkbox",
+        'list': ['Шутер',
+                'Хоррор',
+                'Гонки',
+                'Стратегии',
+                'Симулятор',
+                'Космос и полёты',
+                'Приключения',
+                'Военное',
+                'Песочница',
+                'Выживание',
+                'Головоломка',
+                'Строительство',]
+    },
+    {
+        'title': "developers",
         'type_function': "checkbox",
         'list': ['Шутер',
                 'Хоррор',
@@ -20,17 +40,28 @@ const points = [
         'type_function': "radio",
         'list': ['По популярности',
                 'По названию',
-                'Сначала дешёвые',
-                'Сначала Дорогие',
+                'По цене',
                 'По дате выхода',
                 'По величине скидки',
                 'По рейтингу']
     }
 ]
 
+let filter_values = {
+    'sorting': "",
+    'developers': [],
+    'genres': [],
+}
 
+function reset_filter_values() {
+    filter_values = {
+        'sorting': "",
+        'developers': [],
+        'genres': [],
+    }
+}
 
-function make_input_list_genres(id) {
+function make_input_list(id) {
     let points_html = '';
 
     const arr = points.find(item => item.title === id);
@@ -45,5 +76,31 @@ function make_input_list_genres(id) {
     document.querySelector(`#inputList_points-${id}`).innerHTML = points_html;
 }
 
-make_input_list_genres('sorting');
-make_input_list_genres('genres');
+make_input_list('sorting');
+make_input_list('genres');
+make_input_list('developers');
+
+inp_search_genres.addEventListener('input', () => search_point(inp_search_genres.value, 'genres'));
+inp_search_sorting.addEventListener('input', () => search_point(inp_search_sorting.value, 'sorting'));
+
+// -------- Переключение направления сортировки -------- 
+let sort_dir_top_to_bottom = true;
+const sorting_icon_svg = document.querySelector('.formFilters__sortingBlock__icon__svg');
+
+function toggle_direction_sorting() {
+    if (sort_dir_top_to_bottom) {
+        sorting_icon_svg.classList.remove('formFilters__sortingBlock__icon__svg_topToBottom');
+        sorting_icon_svg.classList.add('formFilters__sortingBlock__icon__svg_BottomToTop');
+        sort_dir_top_to_bottom = false;
+    }
+    else {
+        sorting_icon_svg.classList.remove('formFilters__sortingBlock__icon__svg_BottomToTop');
+        sorting_icon_svg.classList.add('formFilters__sortingBlock__icon__svg_topToBottom');
+        sort_dir_top_to_bottom = true;
+    }
+}
+
+function toggle_subRow() {
+    document.querySelector('.formFilters__row_sub').classList.toggle('formFilters__row_hidden');
+    document.querySelector('.formFilters__menuButtons__allFilters').classList.toggle('formFilters__menuButtons__allFilters_active');
+}
--- scripts/input_list.js
+++ scripts/input_list.js
@@ -10,16 +10,65 @@ const toggle_list = (id) => {
 
 const change_value_list = (id, value, type, i) => {
     if (type == 'radio') {
-        radio_value_list(id, value);
+        change_radio_value_list(id, value);
         return
     }
-    chekbox_value_list(id, i);
+    change_chekbox_value_list(id, i, value);
 }
 
-const chekbox_value_list = (id, i) => {
+const change_chekbox_value_list = (id, i, value) => {
     document.querySelector(`#inputList_points-${id} .inputList_point-${i}`).classList.toggle('inputList__list__body__points__point_check');
+    
+    // ! КАК КОНСТ ПЕРЕМЕННАЯ МАССИВА МЕНЯЕТСЯ? И КАК МЕНЯЕТСЯ МАССИВ
+    const values = filter_values[id];
+
+    const e_index = values.indexOf(value);
+
+    if (e_index !== -1) {
+        values.splice(e_index, 1);
+    } 
+    else {
+        values.push(value);
+    }
+
+    // filter_values[id] = values;
+    console.log(values);
+    console.log(filter_values[id]);
+
+    const str = [...values].join(', ');
+    document.querySelector(`#inputList-${id} .inputList__list__header__values`).innerHTML = str;
 }
 
-const radio_value_list = (id, value) => {
-    document.querySelector(`#inp-${id}`).value = value;
+const change_radio_value_list = (id, value) => {
+    filter_values[id] = value;
+    // console.log(filter_values[id]);
+    document.querySelector(`#inputList-${id} .inputList__list__header__title`).innerHTML = filter_values[id];
+}
+
+// function toggle_element(array, element) {
+//     const e_index = array.indexOf(element);
+
+//     if (e_index !== -1) {
+//         array.splice(e_index, 1);
+//         console.log(array);
+//     } 
+//     else {
+//         array.push(element);
+//         console.log(array);
+//     }
+// } inp_search_region.addEventListener('input', () => make_regions_list(selected_region, inp_search_region.value));
+
+// e.full_title.toLowerCase().includes(str.toLowerCase())
+
+function search_point(value, id) {
+    document.querySelectorAll(`#inputList_points-${id} .inputList__list__body__points__point .inputList__list__body__points__point__value`).forEach(e => {
+        console.log(`${e.innerHTML} - ${value}`);
+        if (!e.innerHTML.toLowerCase().includes(value.toLowerCase())) {
+            e.parentElement.classList.add('inputList__list__body__points__point_hidden');
+        }
+
+        else {
+            e.parentElement.classList.remove('inputList__list__body__points__point_hidden');
+        }
+    })
 }
